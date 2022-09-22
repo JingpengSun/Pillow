4e5e24dccd160ed6ac04aa272ab29b78dcd4ea5e
Jingpeng Sun <hsjp413108@163.com>
2022-09-22 18:25:54 +0800

    Update features.py

--- features.py
+++ features.py
@@ -19,6 +19,10 @@ from librosa.feature import zero_crossing_rate
 from utils import energy
 import matplotlib; matplotlib.use('TkAgg')
 
+from fcmeans import FCM   # pip install fuzzy-c-means
+from sklearn.decomposition import PCA
+from mpl_toolkits.mplot3d import Axes3D
+
 class Audio:
     def __init__(self, path, uid, sfreq=16000):
         self.wav, self.sfreq = librosa.load(f'{path}/{uid}.wav', sr=sfreq)
@@ -56,15 +60,194 @@ class Audio:
 
         return res
 
+    
+    
+def sample_entropy(time_series, sample_length, tolerance=None):
+    """Calculates the sample entropy of degree m of a time_series.
+    This method uses chebychev norm.
+    It is quite fast for random data, but can be slower is there is
+    structure in the input time series.
+    Args:
+    time_series: numpy array of time series
+    sample_length: length of longest template vector
+    tolerance: tolerance (defaults to 0.1 * std(time_series)))
+    Returns:
+    Array of sample entropies:
+      SE[k] is ratio "#templates of length k+1" / "#templates of length k"
+      where #templates of length 0" = n*(n - 1) / 2, by definition
+    Note:
+    The parameter 'sample_length' is equal to m + 1 in Ref[1].
+    References:
+    [1] http://en.wikipedia.org/wiki/Sample_Entropy
+    [2] http://physionet.incor.usp.br/physiotools/sampen/
+    [3] Madalena Costa, Ary Goldberger, CK Peng. Multiscale entropy analysis
+      of biological signals
+    """
+    # The code below follows the sample length convention of Ref [1] so:
+    M = sample_length - 1
+
+    time_series = np.array(time_series)
+    if tolerance is None:
+        tolerance = 0.1 * np.std(time_series)
+
+    n = len(time_series)
+
+    # Ntemp is a vector that holds the number of matches. N[k] holds matches templates of length k
+    Ntemp = np.zeros(M + 2)
+    # Templates of length 0 matches by definition:
+    Ntemp[0] = n * (n - 1) / 2
+
+    for i in range(n - M - 1):
+        template = time_series[i:(i + M + 1)]  # We have 'M+1' elements in the template
+        rem_time_series = time_series[i + 1:]
+
+        search_list = np.arange(len(rem_time_series) - M, dtype=np.int32)
+        for length in range(1, len(template) + 1):
+            hit_list = np.abs(rem_time_series[search_list] - template[length - 1]) < tolerance
+            Ntemp[length] += np.sum(hit_list)
+            search_list = search_list[hit_list] + 1
+
+    sampen = -np.log(Ntemp[1:] / Ntemp[:-1])
+    return sampen
+
+def util_granulate_time_series(time_series, scale):
+    """Extract coarse-grained time series
+    Args:
+    time_series: Time series
+    scale: Scale factor
+    Returns:
+    Vector of coarse-grained time series with given scale factor
+    """
+    n = len(time_series)
+    b = int(np.fix(n/scale))
+    temp = np.reshape(time_series[0:b * scale], (b,scale))
+    cts = np.mean(temp, axis=1)
+    return cts
+
+def multiscale_entropy(time_series, sample_length, tolerance=None, maxscale=None):
+    """Calculate the Multiscale Entropy of the given time series considering
+    different time-scales of the time series.
+    Args:
+    time_series: Time series for analysis
+    sample_length: Bandwidth or group of points
+    tolerance: Tolerance (default = 0.1*std(time_series))
+    Returns:
+    Vector containing Multiscale Entropy
+    """
+
+    if tolerance is None:
+        # We need to fix the tolerance at this level
+        # If it remains 'None' it will be changed in call to sample_entropy()
+        tolerance = 0.1 * np.std(time_series)
+    if maxscale is None:
+        maxscale = len(time_series)
+    mse = np.zeros(maxscale)
+    for i in range(maxscale):
+        temp = util_granulate_time_series(time_series, i + 1)
+        mse[i] = sample_entropy(temp, sample_length, tolerance)[-1]
+    return mse   
+    
+    
+    
+    
+    
+def get_features(audio,sr):
+    feature_set = []
+    # mfcc, zcr, 
+    # mfcc刻画音频静态特征
+    # mfcc的第一个分量与音频的振幅有关，因此为了排除振幅的干扰，可直接舍弃第一个分量
+    mfccs = np.mean(librosa.feature.mfcc(y=audio, sr=sr, hop_length=1024, n_mfcc=30)[1,:],axis=1)  # 提取mfcc特征个数为n_mfcc个 [n_mfcc, frames]
+    # 平均后mfccs的维度为[n_mfcc-1,1]
+    # mfcc的一阶和二阶导数，刻画音频动态特征
+    mfcc_delta = np.mean(librosa.feature.delta(mfccs),axis=1)
+    mfcc_delta2 = np.mean(librosa.feature.delta(mfccs,order=2),axis=1)
+    zcr =librosa.feature.zero_crossing_rate(audio, frame_length=2048, hop_length=1024)  # 维度随声音事件长度而变化
+    mean_zcr = mean(zcr)
+    
+    # multiscale entropy of zcr
+    mse_val = mse(zcr, sample_length=3, maxscale=20) 
+    
+    # 统计特征
+    # 偏度与峰度，计算波形与正态分布的相似性
+    # 考虑到鼾声波形的正半部分分布较为对称，因此可对其正数部分计算偏度和峰度
+    positive_audio = audio[audio>0]
+    skew = stats.skew(positive_audio)  # 偏度
+    kurtosis = stats.kurtosis(positive_audio)  # 峰度
+    
+    # 考虑到鼾声波形中间部分高，两边低，因此可计算中间部分能量占总信号的能量比
+    # 由于此时一阶与二阶范数等价，因此此处使用一阶范数
+    middle_energy_ratio = np.linalg.norm(audio[len(audio)//3:-len(audio)//3], ord = 1)/np.linalg.norm(audio, ord = 1)
+    
+    
+    # 检查特征绝对值，决定是否进行特征归一化处理
+    returen feature_set
+    
+    
+
 if __name__ == '__main__':
 
     reader = Audio('../data/snore/jingpeng', '141')
     feat = reader.get_feat_df(0.1, 0.05)
-    plt.subplot(211)
-    plt.plot(reader.wav)
-    plt.subplot(212)
-    feat['energy'] = savgol_filter(feat['energy'], 11, 9)
-    plt.plot(feat['energy'])
-    plt.plot(feat['zcr'])
+    
+    clips = sed(audio)  # 其中包含了检测得到的若干个长度不一的声音片段
+    
+    clips_feature = []
+    for clip in clips:
+        feat = get_features(clip)
+        clips_feature.append(feat)
+    
+    X = feature_set
+    # PCA 降维
+    pca = PCA(n_components=len(feature_set))  # 先计算一次特征值
+    pca.fit(X)
+    # 输出特征值
+    print(pca.explained_variance_)
+    fig = plt.figure()
+    plt.plot(pca.explained_variance_)
     plt.show()
+    # 输出特征向量
+#     print(pca.components_)
+    
+    decreased_dim = 3
+    pca = PCA(n_components=decreased_dim)  # 降到decreased_dim 维
+    pca.fit(X)
+    
+    # 降维后的数据
+    X_new = pca.transform(X)
+#     print(X_new)
+    # 画图显示数据
+    if decreased_dim == 2
+        fig = plt.figure()
+        plt.scatter(X_new[:, 0], X_new[:, 1], marker='o')
+        plt.show()
+    elif decreased_dim == 3
+        # 三维画图
+        fig = plt.figure()
+        ax = Axes3D(fig, rect=[0, 0, 1, 1], elev=30, azim=20)
+        plt.scatter(X[:, 0], X[:, 1], X[:, 2], marker='o')
+        plt.show()
+        
+    # 聚类实现鼾声检测
+    # fitting the fuzzy-c-means
+    fcm = FCM(n_clusters=2)
+    fcm.fit(X_new)
+    
+    # outputs 
+    fcm_centers = fcm.centers
+    fcm_labels = fcm.predict(X_new)
+    
+    
+    # plot results
+    f, axes = plt.subplots(1, 2, figsize=(11,5))
+    axes[0].scatter(X_new[:,0], X_new[:,1], alpha=.1)
+    axes[1].scatter(X_new[:,0], X_new[:,1], c=fcm_labels, alpha=.1)
+    axes[1].scatter(fcm_centers[:,0], fcm_centers[:,1], marker="+", s=500, c='w')
+    plt.savefig('clustering-output.jpg')
+    plt.show()
+    
+    
+    
+    
+    
+    
     print()
